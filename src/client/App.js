b9b3350928bc29497bb78410903abdf7e054f9a6
Cristian <cristian.rock78@gmail.com>
2018-12-16 11:18:07 -0300

    babel y app.js add

---
+++ src/client/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    render() {
+        return (
+        <div>
+          {/* Navegacion */}
+          <nav className="light-blue darken-4">
+              <div className="container">
+                <a className="brand-logo" href="/">Lalymania</a>
+              </div>
+          </nav>
+
+          <div className="container">
+            <div className="row">
+              <div className="col s5"> columna </div>
+            </div>
+          </div>
+
+        </div>
+
+        );
+    }
+}
+
+export default App;
